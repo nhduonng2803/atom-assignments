357207bf9ce59f05ab40a58fdf09f73e5ceff623
nhduonng2803 <83089388+nhduonng2803@users.noreply.github.com>
2021-05-25 19:18:34 +0700

    Update assignment

---
+++ assignment_2/env_variable.py
@@ -0,0 +1,5 @@
+{
+    "SENDER_EMAIL":"nhduong2803@gmail.com",
+    "PWD_EMAIL":"uftysvbkephsnjgu",
+    "WEATHER_API_KEY":"15fa2ad258f2769152b37b92155e89cf"
+}
---
+++ assignment_2/env_variable_test copy.py
@@ -0,0 +1,5 @@
+{
+    "SENDER_EMAIL":"nnduong2803@gmail.com",
+    "PWD_EMAIL":"1234",
+    "WEATHER_API_KEY":"*********"
+}
